dbf2307dd77cc0502ed8f36946f7e4e25d3696a2
ms.fan2 <ms.fan2@gmail.com>
2011-12-01 22:37:12 +0000

    Fan added: •public PersonInfo displayPerson(int personId); •public MovieInfo displayMovie(int movieId); •public Person[] listAllPersons(int type);

--- project/src/connection/MovieManager.java
+++ project/src/connection/MovieManager.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 import team6.entity.Bill;
 import team6.entity.Movie;
+import team6.entity.MovieInfo;
 import team6.entity.Person;
 import team6.entity.PersonInfo;
 
@@ -192,6 +193,7 @@ public class MovieManager {
 //				System.out.println("toto" + movie.getName());
 				movie.setRentAmount(rs.getDouble("rent_amount"));
 				
+				
 				if (rs.getString("status").equalsIgnoreCase("returned")) {
 					returnedMovies.add(movie);
 				} else if (rs.getString("status").equalsIgnoreCase("rented")) {
@@ -207,4 +209,166 @@ public class MovieManager {
 		}
 		return personInfo;
 	}
+	
+	
+	public PersonInfo displayPerson(int personId) {
+		
+		PersonInfo personInfo = new PersonInfo();
+		try {
+			// Get the person from db
+			String sql = "SELECT * FROM persons p JOIN accounts a ON p.id = a.person_id WHERE p.id = ?";
+			PreparedStatement prepare = conn.prepareStatement(sql);
+			prepare.setInt(1, personId);
+			ResultSet rs = prepare.executeQuery();
+			rs.first();
+			
+			Person person = new Person();
+			person.setId(rs.getInt("id"));
+			person.setDateRegistration(new java.util.Date(rs.getDate("date_registration").getTime()));
+			person.setFirstName(rs.getString("first_name"));
+			person.setLastLogin(new java.util.Date(rs.getDate("last_login").getTime()));
+			person.setLastName(rs.getString("last_name"));
+			person.setPassword(rs.getString("password"));
+			person.setUsername(rs.getString("username"));
+			person.setSsn(rs.getString("ssn"));
+			
+			personInfo.setPerson(person);
+			rs.close();
+			prepare.close();
+			
+			// Get the rented movies for this user
+			sql = "SELECT * "
+					+ "FROM rentals r JOIN movies m ON r.id_movies = m.id "
+					+ "WHERE r.id_persons = ?";
+			
+			prepare = conn.prepareStatement(sql);
+	
+					
+			prepare.setInt(1, personId);
+			
+			rs = prepare.executeQuery();
+			
+			ArrayList<Movie> rentedMovies = new ArrayList<Movie>();
+			ArrayList<Movie> returnedMovies = new ArrayList<Movie>();
+			
+			while (rs.next()) {
+				Movie movie = new Movie();
+				movie.setId(rs.getInt("id_movies"));
+				movie.setName(rs.getString("name"));
+				movie.setBanner(rs.getString("banner"));
+				movie.setReleaseDate(new java.util.Date(rs.getDate("release_date").getTime()));
+				movie.setRentAmount(rs.getDouble("rent_amount"));
+				movie.setNbAvailable(rs.getInt("nb_available"));
+				
+				if (rs.getString("status").equalsIgnoreCase("returned")) {
+					returnedMovies.add(movie);
+				} else if (rs.getString("status").equalsIgnoreCase("rented")) {
+					rentedMovies.add(movie);
+				}
+			}
+			
+			personInfo.setListActualRentMovie(rentedMovies);
+			personInfo.setListRentMovie(returnedMovies);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return personInfo;
+		
+	}
+	
+	public MovieInfo displayMovie(int movieId) {
+		
+		MovieInfo movieInfo = new MovieInfo();
+		
+		Movie movie = null;
+		try {
+			String sql = "SELECT * FROM movies WHERE id = ?";
+			PreparedStatement prepare = conn.prepareStatement(sql);
+			prepare.setInt(1, movieId);
+			ResultSet rs = prepare.executeQuery();
+			if (rs.first()) {
+				movie = new Movie();
+				movie.setId(rs.getInt("id"));
+				movie.setName(rs.getString("name"));
+				movie.setBanner(rs.getString("banner"));
+				movie.setNbAvailable(rs.getInt("nb_available"));
+				movie.setRentAmount(rs.getDouble("rent_amount"));
+				movie.setReleaseDate(new java.util.Date(rs.getDate("release_date").getTime()));
+				
+				movieInfo.setMovie(movie);
+				
+				rs.close();
+				prepare.close();
+				
+				// Get the persons who rented  this movie
+				sql = "select * from rentals r, persons p,  accounts a "
+						+ "where r.id_persons=p.id and p.id=a.person_id and r.id_movies=?";
+				
+				prepare = conn.prepareStatement(sql);
+		
+						
+				prepare.setInt(1, movieId);
+				
+				rs = prepare.executeQuery();
+				
+				ArrayList<Person> rentPersons = new ArrayList<Person>();
+				
+				while (rs.next()) {
+					Person person = new Person();
+					person.setId(rs.getInt("id_persons"));
+					person.setDateRegistration(new java.util.Date(rs.getDate("date_registration").getTime()));
+					person.setFirstName(rs.getString("first_name"));
+					person.setLastLogin(new java.util.Date(rs.getDate("last_login").getTime()));
+					person.setLastName(rs.getString("last_name"));
+					person.setPassword(rs.getString("password"));
+					person.setUsername(rs.getString("username"));
+					person.setSsn(rs.getString("ssn"));
+					
+					rentPersons.add(person);
+									
+				}
+				
+				movieInfo.setListPerson(rentPersons);
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return movieInfo;
+	}
+	
+	public Person[] listAllPersons(int type) {
+		ArrayList<Person> persons = new ArrayList<Person>(); 
+	
+		try {	
+			// Get all persons from db
+			String sql = "SELECT * FROM persons p JOIN accounts a ON p.id = a.person_id";
+			PreparedStatement prepare = conn.prepareStatement(sql);
+			
+			ResultSet rs = prepare.executeQuery();
+			
+			while (rs.next()) {
+				Person person = new Person();
+				person.setId(rs.getInt("id"));
+				person.setDateRegistration(new java.util.Date(rs.getDate("date_registration").getTime()));
+				person.setFirstName(rs.getString("first_name"));
+				person.setLastLogin(new java.util.Date(rs.getDate("last_login").getTime()));
+				person.setLastName(rs.getString("last_name"));
+				person.setPassword(rs.getString("password"));
+				person.setUsername(rs.getString("username"));
+				person.setSsn(rs.getString("ssn"));
+				
+				persons.add(person);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		Person[] arrPersons = new Person[persons.size()];
+		arrPersons = persons.toArray(arrPersons);
+		return arrPersons;
+	}
+
 }
