260857f1ea3ea572e5c6bb620d66ade4fd18d113
ms.fan2@gmail.com <ms.fan2@gmail.com@a0192287-0cf4-bfb1-14a1-5b5d3a494836>
2011-12-02 23:22:18 +0000

    added complete list of services

--- project/src/connection/Services.java
+++ project/src/connection/Services.java
@@ -1,5 +1,8 @@
 package team6.connection;
 
+import java.util.Date;
+
+import team6.entity.Address;
 import team6.entity.Bill;
 import team6.entity.Movie;
 import team6.entity.MovieInfo;
@@ -65,6 +68,106 @@ public interface Services {
 	 * @return Person[], with all information (person) for this type
 	 */
 	public Person[] listAllPersons(int type);
+
+	/**
+	 * Do a research in the database with the criteria submitted
+	 * 
+	 * @param name
+	 * @param banner
+	 * @param release
+	 * @param rentalPrice
+	 * @param category
+	 * @param isAvailable
+	 * @return all the movies that match the search
+	 */
+	public Movie[] findMovies(String name, String banner, Date release,
+			Double rentalPrice, int category, boolean isAvailable);
+
+	/**
+	 * Get all movies from the database
+	 * 
+	 * @return tab of movies
+	 */
+	public Movie[] listMovies();
+
+	/**
+	 * Rent a movie under an user ID
+	 * 
+	 * @param movieId
+	 *            movie to rent
+	 * @param personId
+	 *            renter
+	 * @return result of the operation (ok, denied, etc.)
+	 */
+	public String issueMovie(int movieId, int personId);
+
+	/**
+	 * Return a movie rented by an user
+	 * 
+	 * @param movieId
+	 *            returning movie
+	 * @param personId
+	 *            renter
+	 * @return result of the operation
+	 */
+	public String submitMovie(int movieId, int personId);
+	
+	/**
+	 * Logon a person
+	 * 
+	 * @param user
+	 * @param hashPassword
+	 * @return result of the operation
+	 */
+	public String logon(String user, String hashPassword);   
+	
+	/**
+	 * create a person and account in db
+	 * 
+	 * @param person
+	 * @param accountType
+	 * @param ssn
+	 * @return result of the operation
+	 */
+	public String signUp(Person person, int accountType, String ssn);
+	
+	/**
+	 * logout a person
+	 * 
+	 * @param personId
+	 * @return result of the operation
+	 */
+	public String logOut(int personId);
+
+	/**
+	 * update a person and account in db
+	 * 
+	 * @param person
+	 * @param accountType
+	 * @param ssn
+	 * @return result of the operation
+	 */
+	public String updatePerson(Person person, int accountType, String ssn);
+	
+	/**
+	 * delete a person and account in db
+	 * 
+	 * @param personId
+	 * @return result of the operation
+	 */
+	public String deletePerson(int personId);
+
+	/**
+	 * find persons based on parameters
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param address
+	 * @param type
+	 * @param issuedMovie
+	 * @return result of the operation
+	 */
+	public Person[] findPersons(String firstName, String lastName, Address address, int type, int issuedMovie);
 
 	
 }
