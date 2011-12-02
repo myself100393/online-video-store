b0248d4f238d250581a1160b8754508aa7bd413b
ms.fan2@gmail.com <ms.fan2@gmail.com@a0192287-0cf4-bfb1-14a1-5b5d3a494836>
2011-12-02 23:27:21 +0000

    updated ServicesImpl file

--- project/src/connection/ServicesImpl.java
+++ project/src/connection/ServicesImpl.java
@@ -1,7 +1,10 @@
 package team6.connection;
 
+import java.util.Date;
+
 import javax.jws.WebService;
 
+import team6.entity.Address;
 import team6.entity.Bill;
 import team6.entity.Movie;
 import team6.entity.MovieInfo;
@@ -63,6 +66,78 @@ public class ServicesImpl implements Services {
 		/**This space is left for validation and manipulation of 
 		input values entered by the user as a part of the server side validation*/
 		return movieManager.listAllPersons(type);
+	}
+
+	@Override
+	public Movie[] findMovies(String name, String banner, Date release,
+			Double rentalPrice, int category, boolean isAvailable) {
+		SearchMovieManager smm = new SearchMovieManager();
+		return smm.findMovies(name, banner, release, rentalPrice, category, isAvailable);
+	}
+
+	@Override
+	public Movie[] listMovies() {
+		SearchMovieManager smm = new SearchMovieManager();
+		return smm.listMovies();
+	}
+
+	@Override
+	public String issueMovie(int movieId, int personId) {
+		SearchMovieManager smm = new SearchMovieManager();
+		return smm.issueMovie(movieId, personId);
+	}
+
+	@Override
+	public String submitMovie(int movieId, int personId) {
+		SearchMovieManager smm = new SearchMovieManager();
+		return smm.submitMovie(movieId, personId);
+	}
+
+	@Override
+	public String logon(String user, String hashPassword) {
+		/**This space is left for validation and manipulation of 
+		input values entered by the user as a part of the server side validation*/
+		return movieManager.logon(user, hashPassword);
+	}
+	
+	@Override
+	public String signUp(Person person, int accountType, String ssn) {
+		/**This space is left for validation and manipulation of 
+		input values entered by the user as a part of the server side validation*/
+		
+		return movieManager.signUp(person, accountType, ssn);
+	}
+
+	@Override
+	public String logOut(int personId) {
+		/**This space is left for validation and manipulation of 
+		input values entered by the user as a part of the server side validation*/
+		
+		return movieManager.logOut(personId);
+	}
+
+	@Override
+	public String updatePerson(Person person, int accountType, String ssn) {
+		/**This space is left for validation and manipulation of 
+		input values entered by the user as a part of the server side validation*/
+		
+		return movieManager.updatePerson(person, accountType, ssn);
+	}
+
+	@Override
+	public String deletePerson(int personId) {
+		/**This space is left for validation and manipulation of 
+		input values entered by the user as a part of the server side validation*/
+		
+		return movieManager.deletePerson(personId);
+	}
+
+	@Override
+	public Person[] findPersons(String firstName, String lastName, Address address, int type, int issuedMovie) {
+		/**This space is left for validation and manipulation of 
+		input values entered by the user as a part of the server side validation*/
+		
+		return movieManager.findPersons(firstName, lastName, address, type, issuedMovie);
 	}
 
 
